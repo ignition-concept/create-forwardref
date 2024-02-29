45dabfe2990b7ebaed80211c3f8ee904fbcef5a7
KimHout Sam <samkimhout@gmail.com>
2024-02-29 11:27:54 +0700

    feat: add util and publish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-export {}
+import React from "react";
+
+export function createForwardRef<
+  TProps extends Record<never, never>,
+  TTag extends
+    | keyof React.JSX.IntrinsicElements
+    | React.ForwardRefExoticComponent<any>
+    | React.ComponentType<any>
+>(
+  tag: TTag,
+  render: React.ForwardRefRenderFunction<
+    React.ElementRef<TTag>,
+    Omit<React.ComponentPropsWithoutRef<TTag>, keyof TProps> & TProps
+  >,
+  exactName: string | undefined = undefined
+) {
+  const Forwarded = React.forwardRef(render);
+
+  if (exactName) {
+    Forwarded.displayName = exactName;
+
+    return Forwarded;
+  }
+
+  if (typeof tag === "function") {
+    Forwarded.displayName = `ignitorForwardedComponent(${tag.displayName})`;
+
+    return Forwarded;
+  }
+
+  Forwarded.displayName = `ignitorForwardedTag(${
+    tag as keyof React.JSX.IntrinsicElements
+  })`;
+
+  return Forwarded;
+}
--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -1,5 +1,32 @@
+import { createForwardRef } from "../src/index";
+
 describe("test should works", () => {
-  test("!!", () => {
-    expect(1 + 1).toBe(2);
+  const ForwardedDiv = createForwardRef("div", (props, ref) => {
+    return <div {...props} ref={ref} />;
+  });
+
+  const ForwardedComponent = createForwardRef(ForwardedDiv, (props, ref) => {
+    return <ForwardedDiv {...props} ref={ref} />;
+  });
+
+  const ForwardedExactName = createForwardRef(
+    ForwardedDiv,
+    (props, ref) => {
+      return <ForwardedDiv {...props} ref={ref} />;
+    },
+    "ExactName"
+  );
+
+  it("forwarded only jsx string tag", () => {
+    expect(ForwardedDiv).toBeTruthy();
+  });
+
+  it("forwarded only jsx function tag", () => {
+    expect(ForwardedComponent).toBeTruthy();
+  });
+
+  it("forwarded and pass name", () => {
+    expect(ForwardedExactName).toBeTruthy();
+    expect(ForwardedExactName.displayName).toBe("ExactName");
   });
 });
