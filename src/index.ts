4e190e831fbba47023e9f52f98e2be3ac8d2c213
KimHout Sam <samkimhout@gmail.com>
2024-03-03 22:56:44 +0700

    feat: add functionality that can getting props so javascript user can using

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import React from "react";
 
+export * as PropType from "prop-types";
+
 export function createForwardRef<
   TProps extends Record<never, never>,
   TTag extends
@@ -14,6 +16,11 @@ export function createForwardRef<
   >,
   exactName: string | undefined = undefined
 ) {
+  console.log(
+    "`exactName` argument soon maybe next minor version is going to remove around",
+    "`0.2.0`"
+  );
+
   const Forwarded = React.forwardRef(render);
 
   if (exactName) {
@@ -31,6 +38,71 @@ export function createForwardRef<
   Forwarded.displayName = `ignitorForwardedTag(${
     tag as keyof React.JSX.IntrinsicElements
   })`;
+
+  return Forwarded;
+}
+
+import PropType from "prop-types";
+
+interface OverrideForwardRef<TSchema extends Record<never, never>> {
+  propTypes?: TSchema;
+  displayName?: string;
+  /**
+   * @danger for functional component users
+   * @warning for class component users
+   *
+   * @see {@link https://github.com/facebook/react/pull/16210}
+   */
+  defaultProps?: PropType.InferProps<TSchema>;
+}
+
+/**
+ * @description
+ *
+ * still implement but i prefer you using the old one with caution
+ */
+export function unsafe_createForwardRef<
+  TTag extends
+    | keyof React.JSX.IntrinsicElements
+    | React.ForwardRefExoticComponent<any>
+    | (new (props: any) => React.Component<any, {}, any>)
+    | ((props: any, context?: any) => React.ReactNode),
+  TSchema extends Record<never, never>
+>(
+  tag: TTag,
+  render: React.ForwardRefRenderFunction<
+    React.ElementRef<TTag>,
+    Omit<
+      React.ComponentPropsWithoutRef<TTag>,
+      keyof PropType.InferProps<TSchema>
+    > &
+      PropType.InferProps<TSchema>
+  >,
+  override: OverrideForwardRef<TSchema>
+) {
+  const defaultOverride = {
+    ...override,
+    displayName: override.displayName
+      ? override.displayName
+      : `_forwarded(${tag})`,
+  };
+
+  const Forwarded = React.forwardRef(render);
+
+  Forwarded.displayName = defaultOverride.displayName;
+
+  if (defaultOverride.defaultProps) {
+    console.log(
+      "Warn: for functional components please remove it from your component if you using class Component just ignore"
+    );
+
+    Forwarded.defaultProps =
+      defaultOverride.defaultProps as unknown as typeof Forwarded.defaultProps;
+  }
+
+  if (defaultOverride.propTypes) {
+    Forwarded.propTypes = defaultOverride.propTypes;
+  }
 
   return Forwarded;
 }
